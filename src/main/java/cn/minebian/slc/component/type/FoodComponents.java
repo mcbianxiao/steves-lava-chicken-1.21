2c6c0e27c8d852fcc94cca4b8b4df2e64217a3de
mcbianxiao <2869553981@qq.com>
2025-04-12 23:18:08 +0800

    Added speed effect

--- src/main/java/cn/minebian/slc/component/type/FoodComponents.java
+++ src/main/java/cn/minebian/slc/component/type/FoodComponents.java
@@ -10,6 +10,7 @@ public class FoodComponents {
             .saturationModifier(1.2F)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 60, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1200, 0), 1.0F)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600, 0), 1.0F)
             .build();
     public static final FoodComponent STLEVES_LAVA_CHICKEN_HOT = new FoodComponent.Builder()
             .nutrition(2)
